edab9793845208f9ba767aac8ec9a3f60ecb3756
Soy Frank <ernestinversion@gmail.com>
2024-05-30 11:05:36 -0300

    Starting app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import eurekaeLogo from './images/logo.png'
+import ListaDeTareas from './components/ListaDeTareas';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="aplicacion-tareas">
+      <div className='eurekae-logo-contenedor'>
+        <img src={eurekaeLogo} className='eurekae-logo' />
+      </div>
+      <div className='tareas-lista-principal'>
+        <h1>Mis Tareas</h1>
+        <ListaDeTareas />
+      </div>
     </div>
   );
 }
